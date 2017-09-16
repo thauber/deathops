e03584d0af54a1a7f9e639c68fe5b247ac99d5d7
Matt Billenstein <matt@vazor.com>
2017-09-15 18:56:48 -0700

    Add autossh example

--- tools/autossh.sh
+++ tools/autossh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install autossh
+# make sure autossh is installed first
+# sudo apt-get install autossh
 
 sudo -u mattb /usr/bin/autossh -M 10000 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /home/mattb/.ssh/id_rsa -R 2222:localhost:22 matt@vazor.com &
